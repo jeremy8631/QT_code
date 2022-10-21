68dd7dd7febc612da3dbe1bf7b49054304bf6aa8
jeremy8631 <jeremy8631@163.com>
2022-10-21 08:48:30 +0800

    test

--- QT_test1.py
+++ QT_test1.py
@@ -1,21 +1,48 @@
-from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton,  QPlainTextEdit
-#导入相关的类 
+from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton,  QPlainTextEdit,QMessageBox
 
-app = QApplication([])#创建一个对象  作用事提供整个图形界面的管理
+class Stats():  #类的里面放以下相应的操作
+    def __init__(self):
+        self.window = QMainWindow()
+        self.window.resize(500, 400)
+        self.window.move(300, 300)
+        self.window.setWindowTitle('薪资统计')
 
-window = QMainWindow() #创建一个主窗口对象
-window.resize(500, 400) #窗口大小 （宽度，高度）
-window.move(300, 310) #控制窗口显示的时候出现在屏幕显示器的位置
-window.setWindowTitle('薪资统计') #把薪资统计设置在标题栏上
+        self.textEdit = QPlainTextEdit(self.window)
+        self.textEdit.setPlaceholderText("请输入薪资表")
+        self.textEdit.move(10, 25)
+        self.textEdit.resize(300, 350)
 
-textEdit = QPlainTextEdit(window) #创建纯文本空间
-textEdit.setPlaceholderText("请输入薪资表")#创建提示文本
-textEdit.move(10,25)#在窗口处位置  如果有副窗口就显示在副窗口位置
-textEdit.resize(300,350) #指定大小
+        self.button = QPushButton('统计', self.window)
+        self.button.move(380, 80)
 
-button = QPushButton('统计', window) #设置按键
-button.move(380,80) #位置
+        self.button.clicked.connect(self.handleCalc)
 
-window.show() #显示界面，想要显示就必须要执行这一段代码
 
-app.exec_() #进入QApplication的事件处理循环  如果不加这句话，界面就会一闪而过 
+    def handleCalc(self):
+        info = self.textEdit.toPlainText()
+
+        # 薪资20000 以上 和 以下 的人员名单
+        salary_above_20k = ''
+        salary_below_20k = ''
+        for line in info.splitlines():
+            if not line.strip():
+                continue
+            parts = line.split(' ')
+            # 去掉列表中的空字符串内容
+            parts = [p for p in parts if p]
+            name,salary,age = parts
+            if int(salary) >= 20000:
+                salary_above_20k += name + '\n'
+            else:
+                salary_below_20k += name + '\n'
+
+        QMessageBox.about(self.window,
+                    '统计结果',
+                    f'''薪资20000 以上的有：\n{salary_above_20k}
+                    \n薪资20000 以下的有：\n{salary_below_20k}'''
+                    )
+
+app = QApplication([])  #这个要放在类的外面
+stats = Stats()  
+stats.window.show()
+app.exec_()
